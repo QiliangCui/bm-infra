4cec623d478a3ca67f23f96623e7395c88ee65b5
Aman Gupta <amangu@google.com>
2025-09-26 15:54:34 -0700

    Fixing error where if no crontab was getting set if grep command fails.

--- service/bm-scheduler/install.sh
+++ service/bm-scheduler/install.sh
@@ -48,4 +48,5 @@ echo "sudo systemctl stop bm-scheduler.service"
 sudo systemctl stop bm-scheduler.service
 
 # add to crontab
-(crontab -l 2>/dev/null | grep -v 'bm-scheduler.service'; echo "0 0 * * * sudo /bin/systemctl restart bm-scheduler.service") | crontab -
+(crontab -l 2>/dev/null | (grep -v 'bm-scheduler.service' || true); echo "0 0 * * * sudo /bin/systemctl restart bm-scheduler.service") | crontab -
+echo "Successfully added crontab"
