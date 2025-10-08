d412f46666e4165be4d9096161c11abc16e5163e
Utkarsh Sharma <utkarshsharma1@users.noreply.github.com>
2025-10-08 12:05:47 +0530

    Renamed tpu_commons repo to tpu-inference (#99)
    * Renamed tpu_commons to tpu_inference
    
    * Minor modification
    
    Signed-off-by: Utkarsh Sharma <utksharma@google.com>
    
    * Fixed tpu-inference repo url
    
    ---------
    
    Signed-off-by: Utkarsh Sharma <utksharma@google.com>
    Co-authored-by: Utkarsh Sharma <utksharma@google.com>

--- scripts/agent/docker_run_bm.sh
+++ scripts/agent/docker_run_bm.sh
@@ -11,8 +11,8 @@ ENV_FILE=$1
 source /etc/environment
 source $ENV_FILE
 
-remove_docker_container() { 
-    docker rm -f tpu-test || true; 
+remove_docker_container() {
+    docker rm -f tpu-test || true;
     docker rm -f vllm-tpu || true;
     docker rm -f $CONTAINER_NAME || true;
 }
@@ -24,7 +24,7 @@ remove_docker_container
 
 image_tag=$GCP_REGION-docker.pkg.dev/$GCP_PROJECT_ID/vllm-tpu-bm/vllm-tpu:$CODE_HASH
 
-IFS='-' read -r VLLM_HASH TPU_COMMON_HASH TORCHAX_HASH _ <<< "$CODE_HASH"
+IFS='-' read -r VLLM_HASH TPU_INFERENCE_HASH TORCHAX_HASH _ <<< "$CODE_HASH"
 
 echo "image tag: $image_tag"
 
@@ -42,7 +42,7 @@ REMOTE_LOG_ROOT="gs://$GCS_BUCKET/job_logs/$RECORD_ID/"
 echo "Results will be stored in: $LOG_ROOT"
 
 if [ -z "$HF_TOKEN" ]; then
-  echo "Error: HF_TOKEN is not set or is empty."  
+  echo "Error: HF_TOKEN is not set or is empty."
   exit 1
 fi
 
@@ -76,7 +76,7 @@ if [ -n "$SKIP_JAX_PRECOMPILE" ]; then
 fi
 
 echo "starting docker...$CONTAINER_NAME"
-echo    
+echo
 docker run \
  -v $DOWNLOAD_DIR:$DOWNLOAD_DIR \
  --env-file $ENV_FILE \
@@ -133,7 +133,7 @@ fi
 # =============== temp solution end ===============
 
 # TODO(patemotter): split these into functions
-if [ "$DATASET" = "sharegpt" ]; then  
+if [ "$DATASET" = "sharegpt" ]; then
   echo "Copying dataset to container..."
   mkdir -p ./artifacts/dataset/
   gsutil cp gs://$GCS_BUCKET/dataset/sharegpt/*.* ./artifacts/dataset/
@@ -160,7 +160,7 @@ echo "copy results and logs back..."
 VLLM_LOG="$LOG_ROOT/$TEST_NAME"_vllm_log.txt
 BM_LOG="$LOG_ROOT/$TEST_NAME"_bm_log.txt
 PROFILE_FOLDER="$LOG_ROOT/$TEST_NAME"_profile
-docker cp "$CONTAINER_NAME:/workspace/vllm_log.txt" "$VLLM_LOG" 
+docker cp "$CONTAINER_NAME:/workspace/vllm_log.txt" "$VLLM_LOG"
 docker cp "$CONTAINER_NAME:/workspace/bm_log.txt" "$BM_LOG"
 docker cp "$CONTAINER_NAME:/workspace/profile/plugins/profile" "$PROFILE_FOLDER"
 docker cp "$CONTAINER_NAME:/workspace/failed_output.json" "$LOG_ROOT/failed_output.json" || true
@@ -171,6 +171,8 @@ gsutil cp -r $LOG_ROOT/* $REMOTE_LOG_ROOT
 AccuracyMetricsJSON=$(grep -a "AccuracyMetrics:" "$BM_LOG" | sed 's/AccuracyMetrics: //')
 echo "AccuracyMetricsJSON: $AccuracyMetricsJSON"
 
+if [[ "$DATASET" == "math500" ]]; then
+  # For lm_eval runs, we should focus on the accuracy results only
 LM_EVAL_DATASETS=("math500" "mmlu" "mlperf")
 if [[ " ${LM_EVAL_DATASETS[*]} " =~ " $DATASET " ]]; then
   # For lm_eval runs, we should focus on the accuracy results only
