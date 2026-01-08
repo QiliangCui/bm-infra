c4020bde5e3ecb5174f08e73c74e4ce16eea7e2a
Aman Gupta <amangu@google.com>
2026-01-08 13:01:42 -0800

    Adding a separate scheduler service for daily runs. This should not impact hourly service.

--- service/bm-scheduler/install.sh
+++ service/bm-scheduler/install.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-set -a
-source /etc/environment
-set +a
-
 if ! id -u bm-scheduler >/dev/null 2>&1; then
     echo "sudo useradd -m -s /bin/bash bm-scheduler"
     sudo useradd -m -s /bin/bash bm-scheduler
@@ -30,16 +26,13 @@ password $GITHUB_PERSONAL_ACCESS_TOKEN" | tee -a /home/bm-scheduler/.netrc
 
 echo "git clone https://github.com/QiliangCui/bm-infra.git"
 
-if [[ -z "${BRANCH_NAME}" ]]; then
-  export BRANCH_NAME=main
-fi
-
-git clone --branch "${BRANCH_NAME}" https://github.com/QiliangCui/bm-infra.git
-
 EOF
 
 echo "sudo cp /home/bm-scheduler/bm-infra/service/bm-scheduler/bm-scheduler.service /etc/systemd/system/bm-scheduler.service"
 sudo cp /home/bm-scheduler/bm-infra/service/bm-scheduler/bm-scheduler.service /etc/systemd/system/bm-scheduler.service
+
+echo "sudo cp /home/bm-scheduler/bm-infra/service/bm-scheduler/bm-scheduler-daily.service /etc/systemd/system/bm-scheduler-daily.service"
+sudo cp /home/bm-scheduler/bm-infra/service/bm-scheduler/bm-scheduler-daily.service /etc/systemd/system/bm-scheduler-daily.service
 
 echo "sudo systemctl daemon-reload"
 sudo systemctl daemon-reload
@@ -47,6 +40,10 @@ sudo systemctl daemon-reload
 echo "sudo systemctl stop bm-scheduler.service"
 sudo systemctl stop bm-scheduler.service
 
+echo "sudo systemctl stop bm-scheduler-daily.service"
+sudo systemctl stop bm-scheduler-daily.service
+
 # add to crontab
-(crontab -l 2>/dev/null | (grep -v 'bm-scheduler.service' || true); echo "0 0 * * * sudo /bin/systemctl restart bm-scheduler.service") | crontab -
+(crontab -l 2>/dev/null | (grep -v 'bm-scheduler.service' || true); echo "0 * * * * sudo /bin/systemctl restart bm-scheduler.service") | crontab -
+(crontab -l 2>/dev/null | (grep -v 'bm-scheduler-daily.service' || true); echo "0 0 * * * sudo /bin/systemctl restart bm-scheduler-daily.service") | crontab -
 echo "Successfully added crontab"
