018bc6317d4da140efc8a90bb5bdfb12cc352834
Pate Motter <patemotter@google.com>
2026-01-15 16:23:41 -0800

    Update hash and directory name. (#149)

--- terraform/gcp/infer_test_southamerica_west1/providers.tf
+++ terraform/gcp/infer_test_southamerica_west1/providers.tf
@@ -1,9 +1,9 @@
 provider "google-beta" {
   project = "cloud-tpu-inference-test"
-  region  = "us-central1"
+  region  = "southamerica-west1"
 }
 
 provider "google" {
   project = "cloud-tpu-inference-test"
-  region  = "us-central1"
+  region  = "southamerica-west1"
 }
