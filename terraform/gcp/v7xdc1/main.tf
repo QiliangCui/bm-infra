b72a3fb53131e9f4b6f160950e340c86b6c02195
jyj <yijiaj@google.com>
2026-04-03 18:18:55 +0000

    double tpu7x-8 disk

--- terraform/gcp/v7xdc1/main.tf
+++ terraform/gcp/v7xdc1/main.tf
@@ -12,7 +12,7 @@ module "tpu7x-8" {
   spanner_instance     = var.spanner_instance
   spanner_db           = var.spanner_db
   gcs_bucket           = var.gcs_bucket
-  mnt_disk_gb          = 512
+  mnt_disk_gb          = 1024
   startup_script_path  = "${path.module}/../scripts/startup_v7.sh.tpl"
   branch_hash          = var.branch_hash
   instance_name_offset = var.instance_name_offset
