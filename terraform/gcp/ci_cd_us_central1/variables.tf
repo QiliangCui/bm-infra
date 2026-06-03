b680f1cbc8427dbeccf812dc870d951a5fd445d9
Ming Huang <mhhua@google.com>
2026-06-03 13:24:46 -0700

    Update hash used by TPU workers

--- terraform/gcp/ci_cd_us_central1/variables.tf
+++ terraform/gcp/ci_cd_us_central1/variables.tf
@@ -58,6 +58,6 @@ variable "instance_name_offset" {
 }
 
 variable "branch_hash" {
-  default     = "d86dad249d3b40d624fe0bd9c6a97100c71ca309"
+  default     = "8170231768410009624fcbdcf6855fa51f70a2ad"
   description = "commit hash of bm-infra branch."
 }
